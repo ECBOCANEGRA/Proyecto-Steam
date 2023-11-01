ca14ee0eda52a4af7fa8efc6d2e026fe1c6e9635
ECBOCANEGRA <ecbocanegra@gmail.com>
2023-11-01 15:03:29 -0600

    second commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import pandas as pd
+
+app = FastAPI()
+
+@app.get("/PlayTime")
+def PlayTimeGenre(genero : str ):
+    df_funcion1 = pd.read_parquet('df_funcion1.parquet', engine='fastparquet', index=False)
+    df_funcion1['genres'] = df_funcion1['genres'].astype(str)
+    datos = df_funcion1[df_funcion1['genres'].str.contains(genero, case=False)].head(1)
+    return "Año de lanzamiento con mas horas jugadas para {} : {}".format(genero,datos.iloc[0,1])
+
+@app.get("/UserGenre")
+def UserForGenre(genero : str ):
+    df_funcion2 = pd.read_parquet('df_funcion2.parquet', engine='fastparquet', index=False)
+    df_funcion2['genres'] = df_funcion2['genres'].astype(str)
+    datos = df_funcion2[df_funcion2['genres'].str.contains(genero, case=False)].head(1)
+    return "Usuario con más horas jugadas para {} : {}, Horas jugadas : {}".format(genero,datos.iloc[0,2],datos.iloc[0,3])
+
+
+
+
